c944fdc7fc2e4d9197d8aa1fffbd871ba2a56762
Rich14re <84147059+Rich14re@users.noreply.github.com>
2024-08-23 12:43:08 +0300

    Update settings.py

--- online_course_site/settings.py
+++ online_course_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ayn^2*58pp1bev!pb2#f6656_@u&+2i@x9rdo62(w$p=9br=$6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
