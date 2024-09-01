876406dcb3069bc3b5f84306de29234f8edc9eb7
Rich14re <84147059+Rich14re@users.noreply.github.com>
2024-09-01 12:56:19 +0300

    changed summ

--- static/pay.js
+++ static/pay.js
@@ -1,8 +1,8 @@
 function inputEdit() {
-   document.getElementById("sale").value = 700; 
+   document.getElementById("sale").value = 2; 
    document.getElementById("sale2").value = 700; 
    document.getElementById("sale3").value = 700; 
-   document.getElementById("sale4").value = 700; 
+   document.getElementById("sale4").value = 800; 
     
    document.getElementById("vlt").value = 4100110112906620;
 }
